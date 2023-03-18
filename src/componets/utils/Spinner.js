cf08bccdefde2cf5fe94ea2fd2175a298304b9c4
Zubair Khan Zafar Khan <107192993+Zubair-maker@users.noreply.github.com>
2023-03-18 19:28:36 +0530

    Update Spinner.js

--- src/componets/utils/Spinner.js
+++ src/componets/utils/Spinner.js
@@ -1,9 +1,8 @@
 import React from "react";
 
 const Spinner = () => {
-  return (
-    
-    <div class="spinner-grow "  role="status">
+  return ( 
+   <div class="spinner-grow "  role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
   );
